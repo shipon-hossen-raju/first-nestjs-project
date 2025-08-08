620f0d82fd7d112bc1171d375070cfe5fe177ae8
Shipon Hossen Raju <msshipon234@gmail.com>
2025-08-08 12:35:18 +0600

    controller validation & global validation

--- src/books/book.controller.ts
+++ src/books/book.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post, ValidationPipe,  } from "@nestjs/common";
 import { bookService } from "./book.service";
+import { CreateBookDto } from "./dtos/createBook.dto";
 
-@Controller("book")
+@Controller('book')
 export class bookController {
-   constructor(private readonly bookService: bookService) { }
-   
-   @Get()
-   getBook(): string {
-      return this.bookService.getBook();
-   }
+  constructor(private readonly bookService: bookService) {}
+
+  @Get()
+  getBook(): string {
+    return this.bookService.getBook();
+  }
+
+  @Post()
+// controller validation: createBook(@Body(new ValidationPipe()) body: CreateBookDto): string {
+  createBook(@Body(new ValidationPipe()) body: CreateBookDto): string {
+    console.log('book create data ', body);
+    return this.bookService.createBook(body);
+  }
 }
--- src/books/book.service.ts
+++ src/books/book.service.ts
@@ -4,5 +4,10 @@ export class bookService {
    getBook(): string {
       return "take a book!"
    }
+
+   // create a book
+   createBook(payload): string {
+      return payload;
+   }
 }
 
